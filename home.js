5948dd728b5d104aa77106423dde805604974fc7
srushtibhise <srushti.bhise15@gmail.com>
2022-03-16 19:52:12 -0700

    coupon code

---
+++ home.js
@@ -0,0 +1,14 @@
+
+function changeMode(){
+    var mybody = document.body;
+    mybody.classList.toggle('mydark')
+}
+function loadCoupon(){
+
+  
+    document.getElementById('coupon').style.visibility = 'visible'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden'
+}
